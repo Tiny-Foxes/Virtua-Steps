ce1d7492115fcdc874941b9e98280bc6dce6d093
Ryuto Setsujin <AppleArcade120@gmail.com>
2022-07-21 20:36:57 -0600

    Welcome to v0.1 Virtuality.
    Also branch changes cause yay

--- Graphics/ScreenTitleMenu logo/default.lua
+++ Graphics/ScreenTitleMenu logo/default.lua
@@ -1,16 +1,10 @@
-local MascotEnabled = LoadModule("Config.Load.lua")("ShowMascotCharacter","Save/OutFoxPrefs.ini")
 return Def.ActorFrame{
 	InitCommand=function(self) self:fov(70):zoom(0.5):x(SCREEN_CENTER_X) end,
 	Def.Sprite{
 		Texture= THEME:GetPathG("ScreenTitleMenu","logo/_text"),
 	},
-	LoadActor("_mascot") .. {
-		InitCommand=function(self) self:x(-250):y(30):zoom(1):diffusealpha(0) end,
-		OnCommand=function(self) self:queuecommand("Animate") end,
-		AnimateCommand=function(self) 
-			if MascotEnabled then
-				self:diffusealpha(0):addy(40):decelerate(1):diffusealpha(1):addx(-40)
-			end
-		end
-	}
+	InitCommand=function(self) self:fov(70):zoom(0.5):x(SCREEN_CENTER_X) end,
+	Def.Sprite{
+		Texture= THEME:GetPathG("ScreenTitleMenu","logo/_update"),
+	},
 }
